1b1a0cfbddf5b4d57b33359339802d27c28919e6
Baptiste BESAUT <bbesaut@tanals.com>
2026-06-16 14:49:29 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot en ligne")
+
+@bot.command()
+async def hihi(ctx):
+    await ctx.send("hihi")
+
+bot.run(TOKEN)
